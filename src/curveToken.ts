936c87633a4a6865c8d011e0be77226b88af0771
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-01-10 15:17:29 +0000

    comment how fetching curve pools work

--- src/curveToken.ts
+++ src/curveToken.ts
@@ -47,6 +47,52 @@ async function getFactoryV2Pools(): Promise<{ address: string }[] | undefined> {
   return poolData;
 }
 
+/*
+ * This function is used to find a correspondance between a curve lp token and
+ * the pool address it corresponds to. Unfortunately with the curve ecosystem,
+ * there is little consistency in defining how pools and tokens are related so
+ * a hackish type solution is required. It is somewhat expected that this may
+ * break in the future but as of writing it is the only solution.
+ *
+ * The rationale for finding this correspondance is that we are required to find
+ * the function signature for the add_liquidity and the
+ * remove_liquidity_one_coin functions on the pool address contract. Once we
+ * know the pool address for the given lp token address we can lookup the abi
+ * using etherscan which will have the correct signature
+ *
+ * The logic here is to use three different solutions to find the
+ * correspondance.
+ *
+ * 1. getGauges api endpoint
+ *
+ * The gauge system (see https://dao.curve.fi/gaugeweight) in curve is how curve
+ * token inflation rewards are distributed to LP'ers in various pools. We don't
+ * care exactly about this only that there is an endpoint which lists the pool
+ * information. The returned info contains two items, `swap_token` and `swap`.
+ * The `swap_token` is the lp token for that pool and the `swap` key corresponds
+ * to the pool address. Knowing the lp token address, we can simply iterate
+ * across the list of pool information and match it against the `swap_token`
+ * value.
+ *
+ * 2. getFactoryV2Pools api endpoint
+ *
+ * The getFactoryV2Pools is a list of "metapools" which utilise most often a
+ * pairing of some illiquid stablecoin with highly liquid stablecoins like
+ * DAI/USDT/USDC through the 3CRV token. In these instances, it appears to be
+ * the case that a metapool lp token address is also the pool address so once
+ * we validate that the address is in the list of metapool addresses we can
+ * assume it is also the pool address
+ *
+ * 3. minter() contract function
+ *
+ * In most instances, the contract at the LP token address will have a "minter"
+ * contract function which will return the pool address. Using etherscan, we can
+ * fetch the abi and do a lookup if that function exists or not and if so use it
+ * to get the pool
+ *
+ * If all three solutions listed here fail to find the correspondance, it will
+ * throw an error and a new solution will have to be found in that case
+ */
 async function getCurvePoolFromTokenAddress(address: string): Promise<string> {
   // first try gauge data
   const gaugeData = await getGauges();
@@ -87,7 +133,7 @@ async function getCurvePoolFromTokenAddress(address: string): Promise<string> {
     return await curveLpToken.minter();
   }
 
-  throw new Error("curve pool address could not be found");
+  throw new Error(`curve pool address: ${address} could not be found`);
 }
 
 export async function getCurveTokenInfo<
